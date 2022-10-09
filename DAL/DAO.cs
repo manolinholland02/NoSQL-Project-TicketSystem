5603951811c226e1594ccaa0bfa2cb1d71dc6569
jonathan-mauricio <90071775+jonathan-mauricio@users.noreply.github.com>
2022-10-09 18:17:18 +0200

    Initial application

---
+++ DAL/DAO.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using Model;
+using MongoDB.Bson.Serialization;
+
+namespace DAL
+{
+    public class DAO
+    {
+        private MongoClient client;
+
+        public DAO()
+        {
+            client = new MongoClient("mongodb+srv://GG-Group4:a4oj6VRsNB1T1q3t@gardengroup.nxaaurt.mongodb.net/testa4oj6VRsNB1T1q3t");
+        }
+
+        public List<Databases_Model> GetDatabases()
+        {
+            List<Databases_Model> all_databases = new List<Databases_Model>();
+            
+            foreach (BsonDocument db in client.ListDatabases().ToList())
+            {
+                all_databases.Add(BsonSerializer.Deserialize<Databases_Model>(db));
+            }
+            return all_databases;
+        }
+    }
+
+
+}
---
+++ Model/Databases_Model.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Model
+{
+    public class Databases_Model
+    {
+        [BsonElement("name")]
+        public string name{ get; set; }
+
+        [BsonElement("sizeOnDisk")]
+        public double size { get; set; }
+
+        [BsonElement("empty")]
+        public bool empty { get; set; }
+    }
+}
