ef551f6ffaf9ff432f04d6f980b5e61928bae219
jonathan-mauricio <90071775+jonathan-mauricio@users.noreply.github.com>
2022-10-09 19:48:01 +0200

    Base structure + Updates

---
+++ UI/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+using Logic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace UI
+{
+    public partial class Login : Form
+    {
+        private Databases databases;
+        public Login()
+        {
+            InitializeComponent();
+            databases = new Databases();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            var dbList = databases.Get_All_Databases();
+
+            foreach (var db in dbList)
+            {
+                listBox1.Items.Add(db.name);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Dashboard().ShowDialog();
+            Close();
+        }
+    }
+}
