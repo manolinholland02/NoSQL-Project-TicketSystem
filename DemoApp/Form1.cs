b75c0afb5febf617a60bb1beb332b541afe0bb6a
659045 <659045@student.inholland.nl>
2022-10-09 18:28:03 +0200

    App
    GG project

---
+++ DemoApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Logic;
+using Model;
+
+namespace DemoApp
+{
+    public partial class Form1 : Form
+    {
+        private Databases databases;
+        public Form1()
+        {
+            InitializeComponent();
+            databases = new Databases();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var dbList = databases.Get_All_Databases();
+            
+            foreach (var db in dbList)
+            {
+                listBox1.Items.Add(db.name);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Logic/Databases.cs
@@ -0,0 +1,20 @@
+﻿using DAL;
+using Model;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class Databases
+    {
+        private DAO dao;
+        public Databases()
+        {
+            dao = new DAO();
+        }
+
+        public List<Databases_Model> Get_All_Databases()
+        {
+            return dao.GetDatabases();
+        }
+    }
+}
